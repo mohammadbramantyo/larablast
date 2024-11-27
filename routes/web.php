476eab3d3ae12e8df24693d0de212fe7282bf72e
Mohammad Bramantyo Putra Kusuma <mohammad.bramantyo@ui.ac.id>
2024-11-27 16:36:37 +0700

    add test routes

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,7 @@ Route::get('/', [MasterDataController::class, 'index'])->name('dashboard');
 Route::get('/export-data', [MasterDataController::class, 'export'])->name('export');
 Route::post('/upload', [MasterDataController::class, 'upload'])->name('upload');
 Route::post('/upload-spatie', [MasterDataController::class, 'upload_simple_excel'])->name('upload_simple_excel');
+Route::get('/test-routes', function () {
+    return 'Test route is working!';
+ });
+
