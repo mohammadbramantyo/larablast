4f13204caba05fda709804d3ea7882ecc174af1e
Mohammad Bramantyo Putra Kusuma <mohammad.bramantyo@ui.ac.id>
2024-11-27 16:32:21 +0700

    try new insert

--- app/Http/Controllers/MasterDataController.php
+++ app/Http/Controllers/MasterDataController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\MasterData;
+use App\Models\UploadHistory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Spatie\SimpleExcel\SimpleExcelReader;
@@ -128,7 +129,6 @@ class MasterDataController extends Controller
         $this->saveDataToDatabase($data);
 
         return response()->json(['message' => 'Success mang']);
-
     }
 
     public function upload_simple_excel(Request $request)
@@ -151,51 +151,15 @@ class MasterDataController extends Controller
             ->useDelimiter(';')
             ->noHeaderRow()
             ->getRows()
-            ->skip(1)
-            ->each(function (array $rowProperties) {
-
-                // Get data and handle empty or null or n/a data
-                $nama = $this->handleNA($rowProperties[0]);
-                $dob = $this->handleNA($rowProperties[1]);
-                $alamat_rumah = $this->handleNA($rowProperties[2]);
-                $kec_rmh = $this->handleNA($rowProperties[3]);
-                $kota_rmh = $this->handleNA($rowProperties[4]);
-                $perusahaan = $this->handleNA($rowProperties[5]);
-                $jabatan = $this->handleNA($rowProperties[6]);
-                $alamat_perush = $this->handleNA($rowProperties[7]);
-                $kota_perush = $this->handleNA($rowProperties[8]);
-                $kode_pos = $this->handleNA($rowProperties[9]);
-                $telp_rumah = $this->handleNA($rowProperties[10]);
-                $telp_kantor = $this->handleNA($rowProperties[11]);
-                $hp_2 = $this->handleNA($rowProperties[12]);
-                $hp_utama = $this->handleNA($rowProperties[13]);
-
-                // Format data
-                $formatted_dob = $this->formatDOB($dob);
-                $formatted_hp_utama = $this->formatPhone($hp_utama);
-                $formatted_hp_kedua = $this->formatPhone($hp_2);
+            ->skip(1);
 
 
-                // Save each row to the database
-                MasterData::create([
-                    'nama' => $nama,
-                    'dob' => $formatted_dob,
-                    'alamat_rumah' => $alamat_rumah,
-                    'kec_rmh' => $kec_rmh,
-                    'kota_rmh' => $kota_rmh,
-                    'perusahaan' => $perusahaan,
-                    'jabatan' => $jabatan,
-                    'alamat_perush' => $alamat_perush,
-                    'kota_perush' => $kota_perush,
-                    'kode_pos' => $kode_pos,
-                    'telp_rumah' => $telp_rumah,
-                    'telp_kantor' => $telp_kantor,
-                    'hp_2' => $formatted_hp_kedua,
-                    'hp_utama' => $formatted_hp_utama
-                ]);
-            });
+        $seenHpUtama = [];
+        $validRows = [];
+        $duplicates = [];
 
-        // Delete the file from storage after processing
+        $this->saveDataToDatabase($rows);
+
         Storage::delete($tempPath);
 
         return response()->json(['message' => 'Success mang']);
@@ -279,38 +243,91 @@ class MasterDataController extends Controller
         return response()->download($zipPath)->deleteFileAfterSend(true);
     }
 
-    private function saveDataToDatabase($data)
+    /**
+     * Save a batch of rows to the database.
+     *
+     * This function processes an array of rows (typically extracted from an Excel file),
+     * validates the data if necessary, and inserts the rows into the appropriate database
+     * table in a batch operation for performance optimization.
+     *
+     * @param array $rows An array of rows to be inserted into the database with no headers
+     *                    Each row is expected to be an associative array representing
+     *                    the data to be stored, where keys are the column number.
+     * * Example:
+     * * [
+     *     [
+     *         0 => 'Name',
+     *         1 => '2002-02-02',
+     *         2 => 'Street Address',
+     *         3 => 'Kec_rumah',
+     *         4 => 'Kota_rmh',
+     *         5 => 'perushaan',
+     *         6 => 'jabatn',
+     *         7 => 'alamat_perush',
+     *         8 => 'kota_perush',
+     *         9 => 'kode_pos',
+     *         10 => 'telp_rumah',
+     *         11 => 'telp_kantor',
+     *         12 => 'hp_2',
+     *         13 => 'hp_utama',
+     *     ],
+     * ]
+     *
+     * @return void
+     */
+
+    private function saveDataToDatabase($rows)
     {
-        foreach ($data as $row) {
-            try {
+        $batch = [];
+        foreach ($rows as $row) {
 
-                // Format data
-                $formatted_dob = $this->formatDOB($row['DOB'] ?? null);
-                $formatted_hp_utama = $this->formatPhone($row['Hp_Utama']);
-                $formatted_hp_kedua = $this->formatPhone($row['Hp_2'] ?? null);
+            // Get data and handle empty or null or n/a data
+            $nama = $this->handleNA($row[0]);
+            $dob = $this->handleNA($row[1]);
+            $alamat_rumah = $this->handleNA($row[2]);
+            $kec_rmh = $this->handleNA($row[3]);
+            $kota_rmh = $this->handleNA($row[4]);
+            $perusahaan = $this->handleNA($row[5]);
+            $jabatan = $this->handleNA($row[6]);
+            $alamat_perush = $this->handleNA($row[7]);
+            $kota_perush = $this->handleNA($row[8]);
+            $kode_pos = $this->handleNA($row[9]);
+            $telp_rumah = $this->handleNA($row[10]);
+            $telp_kantor = $this->handleNA($row[11]);
+            $hp_2 = $this->handleNA($row[12]);
+            $hp_utama = $this->handleNA($row[13]);
 
+            // Format data
+            $formatted_dob = $this->formatDOB($dob);
+            $formatted_hp_utama = $this->formatPhone($hp_utama);
+            $formatted_hp_kedua = $this->formatPhone($hp_2);
 
-                // Save each row to the database
-                MasterData::create([
-                    'nama' => $row['Nama'],
-                    'dob' => $formatted_dob,
-                    'alamat_rumah' => $row['Alamat_Rumah'],
-                    'kec_rmh' => $row['Kec_Rmh'],
-                    'kota_rmh' => $row['Kota_Rmh'],
-                    'perusahaan' => $row['Perusahaan'],
-                    'jabatan' => $row['Jabatan'],
-                    'alamat_perush' => $row['Alamat_Perush'],
-                    'kota_perush' => $row['Kota_Perush'],
-                    'kode_pos' => $row['Kode_pos'],
-                    'telp_rumah' => $row['Telp_Rumah'],
-                    'telp_kantor' => $row['Telp_Kantor'],
-                    'hp_2' => $formatted_hp_kedua,
-                    'hp_utama' => $formatted_hp_utama
-                ]);
-            } catch (\Exception $e) {
-                \Log::error('Error saving row: ', $row);
-                \Log::error('Exception: ', ['message' => $e->getMessage()]);
+            $batch[] = [
+                'nama' => $nama,
+                'dob' => $formatted_dob,
+                'alamat_rumah' => $alamat_rumah,
+                'kec_rmh' => $kec_rmh,
+                'kota_rmh' => $kota_rmh,
+                'perusahaan' => $perusahaan,
+                'jabatan' => $jabatan,
+                'alamat_perush' => $alamat_perush,
+                'kota_perush' => $kota_perush,
+                'kode_pos' => $kode_pos,
+                'telp_rumah' => $telp_rumah,
+                'telp_kantor' => $telp_kantor,
+                'hp_2' => $formatted_hp_kedua,
+                'hp_utama' => $formatted_hp_utama
+            ];
+        }
+
+        try {
+            foreach (array_chunk($batch,1000) as $t) {
+                # code...
+                DB::table('master_data')->insert($t);
             }
+        } catch (\Throwable $th) {
+            throw $th;
+            // \Log::error('Error inserting data', $th);
         }
     }
 
@@ -360,7 +377,7 @@ class MasterDataController extends Controller
      */
     private function handleNA(String $value)
     {
-        $invalidValues = ['N/A', 'None', 'Unknown', '', 'n/a','na'];  // Add more as needed
+        $invalidValues = ['N/A', 'None', 'Unknown', '', 'n/a', 'na'];  // Add more as needed
         return in_array($value, $invalidValues) ? null : $value;
     }
 }
--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -2,38 +2,35 @@
 
 namespace App\Providers;
 
+use Laravel\Telescope\Telescope;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Telescope\IncomingEntry;
-use Laravel\Telescope\Telescope;
 use Laravel\Telescope\TelescopeApplicationServiceProvider;
 
 class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
         // Telescope::night();
 
         $this->hideSensitiveRequestDetails();
 
-        $isLocal = $this->app->environment('local');
-
-        Telescope::filter(function (IncomingEntry $entry) use ($isLocal) {
-            return $isLocal ||
-                   $entry->isReportableException() ||
-                   $entry->isFailedRequest() ||
-                   $entry->isFailedJob() ||
-                   $entry->isScheduledTask() ||
-                   $entry->hasMonitoredTag();
+        Telescope::filter(function (IncomingEntry $entry) {
+           return true;
         });
     }
 
     /**
      * Prevent sensitive request details from being logged by Telescope.
+     *
+     * @return void
      */
-    protected function hideSensitiveRequestDetails(): void
+    protected function hideSensitiveRequestDetails()
     {
         if ($this->app->environment('local')) {
             return;
@@ -48,12 +45,26 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
         ]);
     }
 
+
+
+    public function boot()
+    {
+        parent::boot();
+
+        Telescope::isRecording(function () {
+            return true; // Always record all requests
+        });
+    }
+
+
     /**
      * Register the Telescope gate.
      *
      * This gate determines who can access Telescope in non-local environments.
+     *
+     * @return void
      */
-    protected function gate(): void
+    protected function gate()
     {
         Gate::define('viewTelescope', function ($user) {
             return in_array($user->email, [
