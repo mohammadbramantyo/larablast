8a0eb62a2c6f312d135111a6e0cfff1baac352e9
Mohammad Bramantyo Putra Kusuma <mohammad.bramantyo@ui.ac.id>
2024-11-27 16:33:33 +0700

    Revert "try new insert"
    This reverts commit 4f13204caba05fda709804d3ea7882ecc174af1e.

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -2,35 +2,38 @@
 
 namespace App\Providers;
 
-use Laravel\Telescope\Telescope;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Telescope\IncomingEntry;
+use Laravel\Telescope\Telescope;
 use Laravel\Telescope\TelescopeApplicationServiceProvider;
 
 class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
 {
     /**
      * Register any application services.
-     *
-     * @return void
      */
-    public function register()
+    public function register(): void
     {
         // Telescope::night();
 
         $this->hideSensitiveRequestDetails();
 
-        Telescope::filter(function (IncomingEntry $entry) {
-           return true;
+        $isLocal = $this->app->environment('local');
+
+        Telescope::filter(function (IncomingEntry $entry) use ($isLocal) {
+            return $isLocal ||
+                   $entry->isReportableException() ||
+                   $entry->isFailedRequest() ||
+                   $entry->isFailedJob() ||
+                   $entry->isScheduledTask() ||
+                   $entry->hasMonitoredTag();
         });
     }
 
     /**
      * Prevent sensitive request details from being logged by Telescope.
-     *
-     * @return void
      */
-    protected function hideSensitiveRequestDetails()
+    protected function hideSensitiveRequestDetails(): void
     {
         if ($this->app->environment('local')) {
             return;
@@ -45,26 +48,12 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
         ]);
     }
 
-
-
-    public function boot()
-    {
-        parent::boot();
-
-        Telescope::isRecording(function () {
-            return true; // Always record all requests
-        });
-    }
-
-
     /**
      * Register the Telescope gate.
      *
      * This gate determines who can access Telescope in non-local environments.
-     *
-     * @return void
      */
-    protected function gate()
+    protected function gate(): void
     {
         Gate::define('viewTelescope', function ($user) {
             return in_array($user->email, [
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,3 @@ Route::get('/', [MasterDataController::class, 'index'])->name('dashboard');
 Route::get('/export-data', [MasterDataController::class, 'export'])->name('export');
 Route::post('/upload', [MasterDataController::class, 'upload'])->name('upload');
 Route::post('/upload-spatie', [MasterDataController::class, 'upload_simple_excel'])->name('upload_simple_excel');
-Route::middleware(['web'])->get('/telescope-test', function () {
-    return 'Telescope is working!';
-});
